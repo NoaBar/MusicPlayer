e55c4360ccdd145babfcc05d2f7cb119361f4e24
Noah Bar <noahbar@gmail.com>
2018-03-21 21:59:55 +0100

    switch statement to buttons on SongDetailsActivity is added.
    up navigation in SongDetailsActivity is changed to return to the activity that opened it (musicals or disney).

--- app/src/main/java/com/noah/musicplayer/SongDetailsActivity.java
+++ app/src/main/java/com/noah/musicplayer/SongDetailsActivity.java
@@ -3,12 +3,17 @@ package com.noah.musicplayer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class SongDetailsActivity extends AppCompatActivity {
+import static com.noah.musicplayer.R.id.home_from_details;
+
+public class SongDetailsActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     TextView name;
@@ -23,6 +28,15 @@ public class SongDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_details);
+
+        Button home = (Button) findViewById(home_from_details);
+        home.setOnClickListener(this);
+
+        Button musicals = (Button) findViewById(R.id.musicals_from_details);
+        musicals.setOnClickListener(this);
+
+        Button disney = (Button) findViewById(R.id.disney_from_details);
+        disney.setOnClickListener(this);
 
 
         Intent songDetailsIntent = getIntent();
@@ -45,5 +59,39 @@ public class SongDetailsActivity extends AppCompatActivity {
         character.setText(song.getCharacter());
         year.setText(song.getYear());
         coverImage.setImageResource(song.getImage());
+
     }
+
+//Buttons to get to MainActivity/DisneyActivity/MusicalsActivity
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.home_from_details:
+                Intent mainIntent = new Intent(SongDetailsActivity.this, MainActivity.class);
+                SongDetailsActivity.this.startActivity(mainIntent);
+                break;
+            case R.id.musicals_from_details:
+                Intent musicalsIntent = new Intent(SongDetailsActivity.this, MusicalsActivity.class);
+                SongDetailsActivity.this.startActivity(musicalsIntent);
+                break;
+            case R.id.disney_from_details:
+                Intent disneyIntent = new Intent(SongDetailsActivity.this, DisneyActivity.class);
+                SongDetailsActivity.this.startActivity(disneyIntent);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
